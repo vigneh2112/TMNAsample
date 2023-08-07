b3418585c58fa06f39aec6c1067a60bd394dfe6f
vigneh2112 <141035334+vigneh2112@users.noreply.github.com>
2023-08-07 13:56:54 +0530

    Delete MyApplication directory

--- MyApplication/src/main/java/com/application/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.application.modal.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-	public User findByUsernameAndPassword(String username, String password);
-}
